d683bb4e8cebe8e7785a452bac0d925178f0fd9a
w1024ji <w1024ji@gmail.com>
2024-08-04 06:54:38 +0000

    그래프 개념

--- Graph/union.py
+++ Graph/union.py
@@ -2,8 +2,8 @@
 def find_parent(parent, x):
   # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
   if parent[x] != x:
-    return find_parent(parent, parent[x])
-  return x
+    parent[x] = find_parent(parent, parent[x]) # 갱신
+  return parent[x]
 
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
