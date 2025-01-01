172b5c22823697d7bed750bfb4cf3fd06a34e84e
w1024ji <w1024ji@gmail.com>
2025-01-01 09:48:44 +0000

    구현 문제 복습

--- Review2/implementation/lrud.py
+++ Review2/implementation/lrud.py
@@ -22,5 +22,29 @@ while( q ) : # q가 비어있지 않다면
 
 print(x, y)
     
+''' 책 해설 ''
 
+n = int(input())
+x,y = 1, 1
+plans = input().split()
 
+# LRUD에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+# 이동 계획을 하나씩 확인
+for plan in plans:
+  for i in range(len(move_types)):
+    if plan == move_types[i]:
+      nx = x + dx[i]
+      ny = y + dy[i]
+  # 공간을 벗어나는 경우 무시
+  if nx < 1 or ny < 1 or nx > n or ny > n: # 채택할 것인지 버릴것인지 결정
+    continue
+  # 이동 수행
+  x, y = nx, ny # 채택한다
+
+print(x, y)
+
+'''
--- Review2/implementation/time.py
+++ Review2/implementation/time.py
@@ -13,4 +13,21 @@ for i in range(n+1):
         count += 1
 
 print(count)
-    
+
+''' 책 해설 ''
+-- 이런 유형은 완전 탐색 Brute Forcing 유형으로 분류되기도 한다.
+    -- 완전 탐색은 알고리즘은 비효율적인 시간 복잡도를 가지고 있어서 
+    -- 탐색해야 할 전체 데이터의 개수가 100만 개 이하일 때 사용하면 적절하다
+
+h = int(input())
+
+count = 0
+for i in range(h+1):
+  for j in range(60):
+    for k in range(60):
+      if '3' in str(i) + str(j) + str(k):
+        count += 1
+
+print(count)
+
+'''
