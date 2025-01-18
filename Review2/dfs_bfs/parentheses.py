27b8491dd37d5c9378c524a967f114171abf3053
w1024ji <w1024ji@gmail.com>
2025-01-18 07:36:22 +0000

    [DFS_BFS] 괄호 변환 설명

--- Review2/dfs_bfs/parentheses.py
+++ Review2/dfs_bfs/parentheses.py
@@ -1,5 +1,6 @@
 # [DFS_BFS] [괄호 변환]
 
+# 균형잡힌 괄호 문자열의 인덱스 리턴
 def balanced_index(p):
     count = 0
     for i in range(len(p)):
@@ -10,31 +11,35 @@ def balanced_index(p):
         if count == 0:
             return i
 
+# 올바른지 판단 (True or False)
 def check_proper(p):
     count = 0
     for i in p:
         if i == '(':
             count +=1
         else:
-            if count ==0:
+            if count ==0:       # )로 시작한다면 False 반환
                 return False
             count -=1
     return True
 
+# p를 올바르게 고치는 함수
 def solution(p):
     answer = ''
     if p == '':
         return answer
-    index = balanced_index(p)
+    index = balanced_index(p)   # 균형잡힌 괄호의 인덱스 리턴
     u = p[ : index+1]
     v = p[index+1 : ]
+    # u가 올바르다면, v만 고치면 된다 -> solution(v)
     if check_proper(u):
         answer = u + solution(v)
+    # u가 올바르지 않다면
     else:
         answer = '('
         answer += solution(v)
         answer += ')'
-        u = list(u[1:-1])
+        u = list(u[1:-1]) # 첫 번째와 마지막 괄호를 지운 나머지만 킵한다
         for i in range(len(u)):
             if u[i] == '(':
                 u[i] = ')'
