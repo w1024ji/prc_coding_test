9dbb778095f79ad525b1ab1acb13fcc67132ce6a
w1024ji <w1024ji@gmail.com>
2025-01-25 09:51:09 +0000

    [Greedy] 곱하기 혹은 더하기 답

--- Review2/greedy/multi_or_plus.py
+++ Review2/greedy/multi_or_plus.py
@@ -19,6 +19,16 @@ for s in s_int:
 print(max_sum)
 
 
-
-
+'''
+# 나랑 다른 답안지
+data = input()
+result = int(data[0])
+for i in range(1, len(data)):
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+print(result)
+'''
 
